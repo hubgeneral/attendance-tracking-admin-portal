a602d196c57ea9383d219b15d7d0ee4643b652e8
Semira <nyarkosemiratunatasha@gmail.com>
2025-09-15 16:04:51 +0000

    modify layout import

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "./layout/layout";
 import Dashboard from "./pages/Dashboard";
 import Attendance from "./pages/Attendance";
 import Settings from "./pages/Settings";
 import ManualLogs from "./pages/ManualLogs";
+import Layout from "./layout/Layout";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+      
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/attendance" element={<Attendance />} />
         <Route path="/settings" element={<Settings />} />
