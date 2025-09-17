920c1852e02f0af4fa9f0134d5247a1c2cdfbe54
illusionalgod <justinsane28@protonmail.com>
2025-09-17 04:27:37 -0700

    chore: remove Cypress configuration and update dependencies

---
+++ cypress.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    supportFile: 'cypress/support/e2e.ts',
+    specPattern: [
+      'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+      'cypress/e2e/**/*.js',
+    ],
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+  },
+})
+
