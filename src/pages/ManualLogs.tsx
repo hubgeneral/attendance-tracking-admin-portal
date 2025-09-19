74e4b8e1eeca5c5d6c32942dc15c2999c9b36b67
Dennis <asiedudennis30@gmail.com>
2025-09-19 13:41:07 +0100

    created the Manual Log Page

--- src/pages/ManualLogs.tsx
+++ src/pages/ManualLogs.tsx
@@ -1,11 +1,21 @@
+import { Typography } from "@mui/material";
+import RecentRequests from "../components/recentRequest";
+import LogHistory from "../components/logHistory";
 
-const ManualLogs = () => {
+
+export default function ManualLogs() {
   return (
-    <div>
-      <h1>Manual Logs</h1>
+    <div className="p-4 md:p-6 bg-gray-50 min-h-screen">
+
+      <div className="mb-6">
+        <Typography variant="h4" component="h1" className="font-semibold text-gray-900">
+          Manual Logs
+        </Typography>
+      </div>
+
+      {/* =========== Component Sections ============= */}
+      <RecentRequests />
+      <LogHistory />
     </div>
-  )
+  );
 }
-
-export default ManualLogs
-
