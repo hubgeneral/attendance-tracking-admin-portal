91b74a847af7cdf5f4244bae435b0c3db5592e71
illusionalgod <justinsane28@protonmail.com>
2025-09-27 02:31:28 -0700

    update for e2e login

--- cypress/e2e/pages/login.cy.js
+++ cypress/e2e/pages/login.cy.js
@@ -1,15 +1,22 @@
 !describe('Login Page', () => {
   beforeEach(() => {
-    cy.visit('/login')
+    cy.visit('http://localhost:5173/')
   })
   it('should display the login page', () => {
-    cy.url().should('eq', 'http://localhost:5173/login');
-    cy.get('[placeholder="Email"]').should('be.visible');
-    cy.get('[placeholder="Password"]').should('be.visible');
+    cy.url().should('eq', 'http://localhost:5173/');
+    cy.get('input[type="email"]').should('be.visible');
+    cy.get('input[type="password"]').should('be.visible');
     cy.get('.loginbtn').should('be.visible');
     cy.get('.loginbtn').should('have.text', 'Login');
   })
-})
+
+  it('user can enter email, password and login', ()=> {
+    cy.get('input[type="email"]').type('sampleUser@gmail.com');
+    cy.get('input[type="password"]').type('samplePass');
+    cy.get('.loginbtn').click();
+    cy.url('http://localhost:5173/app/dashboard')
+  })
+});
 
 
 
