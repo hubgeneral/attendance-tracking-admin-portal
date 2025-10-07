2489ae99eabd952d601ffbc0cb79b46a20da9016
Joshua <ngmerteyjoshua360@gmail.com>
2025-10-07 12:02:26 +0000

    Created the reset password pages and its route

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,24 @@ import ManualLogs from "./pages/ManualLogs";
 import Layout from "./layout/Layout";
 import Login from "./pages/Login";
 import UsersPage from "./pages/settings/Users";
+import ResetPassword from "./components/ResetPassword";
+import ResetPasswordConfirmation from "./components/ResetPasswordConfirmation";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
-      <Route path="/app" element={<Layout />} >
+      <Route path="reset-password" element={<ResetPassword />} />
+      <Route
+        path="reset-password-confirmation"
+        element={<ResetPasswordConfirmation />}
+      />
+      <Route path="/app" element={<Layout />}>
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="attendance" element={<Attendance />} />
         <Route path="settings" element={<Settings />} />
         <Route path="logs" element={<ManualLogs />} />
         <Route path="/app/settings/users" element={<UsersPage />} />
-
       </Route>
     </Routes>
   );
